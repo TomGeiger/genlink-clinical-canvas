89fcd6068167fa924ef9b8e7cf6b77cf7b4bb427
Thomas Geiger <tom.geiger@me.com>
2025-07-19 16:13:05 -0400

    Refactor main.tsx to integrate BrowserRouter for routing and set base path

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+
+import { BrowserRouter } from 'react-router-dom';
+
+const basePath = import.meta.env.BASE_URL; // Vite injects this automatically
+
+createRoot(document.getElementById('root')!)
+  .render(
+    <BrowserRouter basename={basePath}>
+      <App />
+    </BrowserRouter>
+  );
+
+
